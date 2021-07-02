5547e68547563a00bff4638596e54fcebaae843a
JZW <qq710327563@126.com>
2021-07-02 15:50:52 +0800

    fix ACMIO

--- LeetCode/substring/src/ACMIO.java
+++ LeetCode/substring/src/ACMIO.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ACMIO {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String content = null;
         String[] t = br.readLine().trim().split(" ");
